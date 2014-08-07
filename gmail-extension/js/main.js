fef025fe300f8051441ee94270409b264158b6e9
duncangraham <duncangraham75@gmail.com>
2014-08-07 18:58:48 -0400

    working wikipedia api usage

--- gmail-extension/js/main.js
+++ gmail-extension/js/main.js
@@ -1,45 +1,54 @@
-window.parseWiki = function (data) {
-  console.log(data.query.pages);
-}
-
-function getWiki () {
-  var script = document.createElement('script');
-  script.src = "https://en.wikipedia.org/w/api.php?action=query&generator=random&prop=extracts&exchars=500&format=json&callback=parseWiki&requestid=" + Math.floor(Math.random()*999999).toString();
-  script.type = "text/javascript";
-
-  document.getElementsByTagName('head')[0].appendChild(script);
-}
-
-var randomWiki;
-
-$.getJSON("https://en.wikipedia.org/w/api.php?action=query&generator=random&prop=extracts&exchars=500&format=json", 
-    function(data) {
-      var obj = data.query.pages;
-      var page = Object.keys(obj);
-      var pageTitle = obj[page[0].title];
-      
-      $.getJSON('https://en.wikipedia.org/w/api.php?action=parse&page='+ pageTitle +'&format=json&prop=text&section=0', 
-        function(data){
-        console.log(data);
-        });
-
-    }
-).fail(function() {
-      console.log( "error" );
-        });
-
-//markov = new Markov({
-//  inputText: inputText
-//});
+var randomWiki,
+    markov;
 
 window.onload = function () {
-  stega()
+  getWiki();
 };
   
 //listen for a URL change
 window.onhashchange = function () {
-  stega()
+  getWiki()
 };
+
+
+
+function getWiki () {
+  $.getJSON("https://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&exchars=500&format=json", 
+      function(data) {
+        var obj = data.query.pages;
+        
+        var pageTitle = getTitle(obj);
+        
+        $.getJSON('https://en.wikipedia.org/w/api.php?action=parse&page='+ pageTitle +'&format=json&prop=text&section=0', 
+          function(data){
+
+            var obj = data.parse.text;
+            var regex = /(<([^>]+)>)/ig;
+            var page = Object.keys(obj);
+            var text = obj[page[0]];
+            randomWiki = text.replace(regex, '');
+            console.log(randomWiki)
+
+            markov = new Markov({
+              inputText: randomWiki
+            });
+            
+            stega();
+          });
+
+
+      }
+  ).fail(function() {
+        console.log( "error" );
+  });
+}
+
+function getTitle(obj) {
+  for( prop in obj ) {
+    var value = obj[prop];
+    return value.title;
+  } 
+}
 
 //inserts dummy text into email body, effectively hiding your own words in a sea of gibberish
 function stega () {
